43a937a01b745e14b4de00f2e7fc12b11da64b19
Hakan Meva <hakan.meva@yahoo.ro>
2023-03-18 17:17:57 +0000

    Add week3 config

--- utilities/query.py
+++ utilities/query.py
@@ -12,10 +12,17 @@ import pandas as pd
 import fileinput
 import logging
 
+#used for model prediction
+import fasttext
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
+
+
+#load fasttext model
+q_classifier = fasttext.load_model('/workspace/datasets/fasttext/model.bin')
 
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
@@ -186,11 +193,19 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
+def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", relev_categories=None):
     #### W3: classify the query
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+
+    filters = {
+        "terms": {
+            "categoryPathIds": relev_categories
+        }
+    }
+
+    query_obj = create_query(user_query, click_prior_query=None, filters=filters, sort=sort, sortDir=sortDir, source=["name", "shortDescription", "categoryPathIds"])
+
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
@@ -245,7 +260,20 @@ if __name__ == "__main__":
         query = line.rstrip()
         if query == "Exit":
             break
-        search(client=opensearch, user_query=query, index=index_name)
+        
+        #predict categories and include in search query filter
+        relev_categories = []
+        total_score = 0
+        ret_cats = q_classifier.predict(query, k=5)
+        print(ret_cats)
+        for idx, cat in enumerate(ret_cats[0]):
+            relev_categories.append(cat.replace("__label__", ""))
+            total_score = total_score + ret_cats[1][idx]
+            if total_score > 0.5:
+                break
+
+        print(relev_categories)
+        search(client=opensearch, user_query=query, index=index_name, relev_categories=relev_categories)
 
         print(query_prompt)
 
--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -7,7 +7,10 @@ import csv
 
 # Useful if you want to perform stemming.
 import nltk
+from tqdm import tqdm
+#import stemmer and tokenizer
 stemmer = nltk.stem.PorterStemmer()
+tokenizer = nltk.tokenize.RegexpTokenizer('\w+')
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
@@ -50,12 +53,40 @@ queries_df = queries_df[queries_df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
 
-# IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+#lowercase queries
+queries_df['lr_query'] = queries_df['query'].str.lower()
 
+#keep only alphanumerical characters
+queries_df['alphanum_query'] = queries_df['lr_query'].str.replace('[^a-z0-9\w]', ' ', regex=True)
+
+#strip queries
+queries_df['stripped_query'] = queries_df['alphanum_query'].apply(lambda x: x.strip())
+
+#apply stemmer on each token and combine query back from tokens
+queries_df['split_query'] = queries_df['stripped_query'].str.split()
+queries_df['final_query'] = queries_df['split_query'].apply( lambda word_list: ' '.join([stemmer.stem(word) for word in word_list ]))
+
+#print(queries_df[queries_df['query'] == 'Beats By Dr. Dre- Monster Pro Over-the-Ear Headphones -' ].final_query)
+
+
+# IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+unpop_cats = queries_df[queries_df['category'].map(queries_df['category'].value_counts()) < 10001]['category'].unique()
+while len(unpop_cats) > 0:
+
+    unpop_cats = queries_df[queries_df['category'].map(queries_df['category'].value_counts()) < 10001]['category'].unique()
+    print(len(unpop_cats))
+
+    for cat in tqdm(unpop_cats):
+        if cat != 'cat00000':
+            parent = parents_df[parents_df['category'] ==  cat ]['parent'].iloc[0]
+            queries_df.loc[queries_df['category'] == cat, 'category'] = parent
+    unpop_cats = queries_df[queries_df['category'].map(queries_df['category'].value_counts()) < 10001]['category'].unique()
+
+print(len(queries_df.category.unique()))
 # Create labels in fastText format.
 queries_df['label'] = '__label__' + queries_df['category']
 
 # Output labeled query data as a space-separated file, making sure that every category is in the taxonomy.
 queries_df = queries_df[queries_df['category'].isin(categories)]
-queries_df['output'] = queries_df['label'] + ' ' + queries_df['query']
+queries_df['output'] = queries_df['label'] + ' ' + queries_df['final_query']
 queries_df[['output']].to_csv(output_file_name, header=False, sep='|', escapechar='\\', quoting=csv.QUOTE_NONE, index=False)
